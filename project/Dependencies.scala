0784bb53a91bdf0aeb660de8b13652c283e17d89
Alexandre Masselot <amasselot@octo.com>
2017-02-16 06:24:08 +0100

    remove Haddop/spark//kafka depenendices

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -2,28 +2,16 @@ import sbt._
 
 object Version {
   val akka = "2.4.11"
-  val reactiveKafka = "0.12"
-  val hadoop = "2.7.3"
-  val logback = "1.1.3"
-  val mockito = "1.10.19"
   val scala = "2.11.8"
   val scalaTest = "2.2.5"
   val slf4j = "1.7.6"
-  val spark = "1.6.0"
 }
 
 object Library {
   val akkaActor = "com.typesafe.akka" %% "akka-actor" % Version.akka
   val akkaTestKit = "com.typesafe.akka" %% "akka-testkit" % Version.akka
-  val hadoopClient = "org.apache.hadoop" % "hadoop-client" % Version.hadoop
-  val logbackClassic = "ch.qos.logback" % "logback-classic" % Version.logback
-  val mockitoAll = "org.mockito" % "mockito-all" % Version.mockito
   val scalaTest = "org.scalatest" %% "scalatest" % Version.scalaTest
   val slf4jApi = "org.slf4j" % "slf4j-api" % Version.slf4j
-  val sparkStreaming = "org.apache.spark" %% "spark-streaming" % Version.spark
-  //val sparkStreamingKafka = "org.apache.spark" %% "spark-streaming-kafka" % Version.spark
-  val reactiveKafka  = "com.typesafe.akka" %% "akka-stream-kafka" % Version.reactiveKafka
-  //  val sparkStreamingAkka = "org.apache.spark"  %% "spark-streaming-akka" % Version.spark
 }
 
 object Dependencies {
@@ -31,15 +19,8 @@ object Dependencies {
   import Library._
 
   val sparkAkkaHadoop = Seq(
-    sparkStreaming,
-    //sparkStreamingKafka,
     akkaActor,
     akkaTestKit,
-    hadoopClient,
-    reactiveKafka,
-//    sparkStreamingAkka,
-    logbackClassic % "test",
-    scalaTest % "test",
-    mockitoAll % "test"
+    scalaTest % "test"
   )
 }
--- src/main/scala/ch/octo/cffpoc/gtfs/simulator/SimulatorToSinkApp.scala
+++ src/main/scala/ch/octo/cffpoc/gtfs/simulator/SimulatorToSinkApp.scala
@@ -1,68 +1,68 @@
-package ch.octo.cffpoc.gtfs.simulator
-
-import java.io.{ File, PrintWriter }
-
-import akka.actor.{ ActorRef, ActorSystem, Props }
-import akka.kafka.ProducerSettings
-import akka.kafka.scaladsl.Producer
-import akka.stream.ActorMaterializer
-import akka.stream.OverflowStrategy._
-import akka.stream.scaladsl.{ Flow, Source }
-import ch.octo.cffpoc.gtfs.{ AgencyName, GTFSSystem, RouteShortName }
-import ch.octo.cffpoc.gtfs.raw.RawCalendarDateReader
-import ch.octo.cffpoc.gtfs.simulator.actors.{ ActorSimulatedTrips, ActorSinkPositions }
-import org.apache.commons.logging.LogFactory
-import org.apache.kafka.clients.producer.ProducerRecord
-import org.apache.kafka.common.serialization.{ ByteArraySerializer, StringSerializer }
-
-import scala.concurrent.ExecutionContext
-
-/**
- * Created by alex on 07.10.16.
- */
-class SimulatorToSinkApp extends SimulatorAppTrait {
-  val LOGGER = LogFactory.getLog(SimulatorToSinkApp.getClass)
-  val path = "src/main/resources/gtfs_gondola"
-
-  def actorRefKafkaSink(system: ActorSystem)(implicit executioncontext: ExecutionContext, materilizer: ActorMaterializer): ActorRef = {
-
-    val producerSettings = ProducerSettings(system, new ByteArraySerializer, new StringSerializer)
-      .withBootstrapServers("localhost:9092")
-
-    val source = Source.actorRef[SimulatedPosition](Int.MaxValue, fail)
-
-    val ref = Flow[ProducerRecord[Array[Byte], String]]
-      .to(Producer.plainSink(producerSettings))
-      .runWith(source.map({ elem =>
-
-        new ProducerRecord[Array[Byte], String]("simulated-positions", elem.toString)
-      }))
-
-    ref
-  }
-
-  def ActorRefPrintSink(system: ActorSystem)(implicit executioncontext: ExecutionContext) = {
-    system.actorOf(Props(new ActorSinkPositions()), "position-sink")
-  }
-
-  def run() = {
-    implicit val system = ActorSystem("GTFSSimulator")
-    implicit val materializer = ActorMaterializer()
-
-    import system.dispatcher
-
-    val date = RawCalendarDateReader.dateFromString("20161005")
-    val trips = loadTripsForDate(date)
-    LOGGER.info(s"transforming ${trips.size} trips")
-
-    val ta = TimeAccelerator(System.currentTimeMillis(), 100, 1000)
-    val actorSink = actorRefKafkaSink(system)
-    val actorSimulatedTrips = system.actorOf(Props(new ActorSimulatedTrips(actorSink, ta, trips, date, 20)))
-  }
-}
-
-object SimulatorToSinkApp extends App {
-  val app = new SimulatorToSinkApp()
-  app.run()
-}
-
+//package ch.octo.cffpoc.gtfs.simulator
+//
+//import java.io.{ File, PrintWriter }
+//
+//import akka.actor.{ ActorRef, ActorSystem, Props }
+//import akka.kafka.ProducerSettings
+//import akka.kafka.scaladsl.Producer
+//import akka.stream.ActorMaterializer
+//import akka.stream.OverflowStrategy._
+//import akka.stream.scaladsl.{ Flow, Source }
+//import ch.octo.cffpoc.gtfs.{ AgencyName, GTFSSystem, RouteShortName }
+//import ch.octo.cffpoc.gtfs.raw.RawCalendarDateReader
+//import ch.octo.cffpoc.gtfs.simulator.actors.{ ActorSimulatedTrips, ActorSinkPositions }
+//import org.apache.commons.logging.LogFactory
+//import org.apache.kafka.clients.producer.ProducerRecord
+//import org.apache.kafka.common.serialization.{ ByteArraySerializer, StringSerializer }
+//
+//import scala.concurrent.ExecutionContext
+//
+///**
+// * Created by alex on 07.10.16.
+// */
+//class SimulatorToSinkApp extends SimulatorAppTrait {
+//  val LOGGER = LogFactory.getLog(SimulatorToSinkApp.getClass)
+//  val path = "src/main/resources/gtfs_gondola"
+//
+//  def actorRefKafkaSink(system: ActorSystem)(implicit executioncontext: ExecutionContext, materilizer: ActorMaterializer): ActorRef = {
+//
+//    val producerSettings = ProducerSettings(system, new ByteArraySerializer, new StringSerializer)
+//      .withBootstrapServers("localhost:9092")
+//
+//    val source = Source.actorRef[SimulatedPosition](Int.MaxValue, fail)
+//
+//    val ref = Flow[ProducerRecord[Array[Byte], String]]
+//      .to(Producer.plainSink(producerSettings))
+//      .runWith(source.map({ elem =>
+//
+//        new ProducerRecord[Array[Byte], String]("simulated-positions", elem.toString)
+//      }))
+//
+//    ref
+//  }
+//
+//  def ActorRefPrintSink(system: ActorSystem)(implicit executioncontext: ExecutionContext) = {
+//    system.actorOf(Props(new ActorSinkPositions()), "position-sink")
+//  }
+//
+//  def run() = {
+//    implicit val system = ActorSystem("GTFSSimulator")
+//    implicit val materializer = ActorMaterializer()
+//
+//    import system.dispatcher
+//
+//    val date = RawCalendarDateReader.dateFromString("20161005")
+//    val trips = loadTripsForDate(date)
+//    LOGGER.info(s"transforming ${trips.size} trips")
+//
+//    val ta = TimeAccelerator(System.currentTimeMillis(), 100, 1000)
+//    val actorSink = actorRefKafkaSink(system)
+//    val actorSimulatedTrips = system.actorOf(Props(new ActorSimulatedTrips(actorSink, ta, trips, date, 20)))
+//  }
+//}
+//
+//object SimulatorToSinkApp extends App {
+//  val app = new SimulatorToSinkApp()
+//  app.run()
+//}
+//
